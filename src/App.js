0c4b0e40a1465cf280f0a883dfb80f3ed65865c8
oksananaumova <oksana.naumova@mustpay.tech>
2023-10-11 21:36:35 -0400

    updated site url

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import { TasksList } from "./pages/taskList/TasksList";
 import { Task } from "./components/task/Task";
 import { CreateTask } from "./pages/createTask/CreateTask";
-import { EditTask } from "./pages/editTask/EditTask";
 import "./app.css";
+import { EditTask } from "./pages/EditTask/EditTask";
 
 export const App = () => {
   
@@ -13,7 +13,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<TasksList />} />
         <Route path="/:id" element={<Task />} />
-        <Route path="/:id/edit" element={<EditTask />} />
+        <Route path="/:id/edit" element={ <EditTask/>} />
         <Route path="/create" element={<CreateTask />} />
       </Routes>
     </div>
